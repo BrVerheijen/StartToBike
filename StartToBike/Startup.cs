4fb82f1ee682be384f790871309aaaef7f5f2ac0
Bram Verheijen <2063119verheijen@zuyd.nl>
2021-10-14 20:01:40 +0200

    Add project files.

---
+++ StartToBike/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StartToBike.Startup))]
+namespace StartToBike
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
