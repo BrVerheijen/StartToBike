59baa67748001e899121784a9f4bc5eb2cd49cac
Bram Verheijen <2063119verheijen@zuyd.nl>
2021-10-19 10:59:23 +0200

    added basic blueprint of classes

---
+++ StartToBike/Models/Injury.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StartToBike.Models
+{
+    public class Injury
+    {
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public string Prevention { get; set; }
+        public string Treatement { get; set; }
+        public ICollection<ApplicationUser> UserList { get; set; }
+    }
+}
